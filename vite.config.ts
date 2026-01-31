439ec7ae135a242261b81662afd2cd6e6f25f018
Gusuder <toda.18@uni-dubna.ru>
2026-01-31 22:35:57 +0300

    Refactor: Move project to root

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,12 @@
 import { defineConfig } from 'vite';
-// @ts-ignore
 import { crx, defineManifest } from '@crxjs/vite-plugin';
 
-// Описываем манифест прямо здесь — так мы получаем типизацию!
 const manifest = defineManifest({
   manifest_version: 3,
   name: 'Shiki Player TS',
-  version: '2.0.0',
-  description: 'Video player integration for Shikimori (Built with Vite)',
-  permissions: [], // Позже добавим 'storage' для настроек
+  version: '1.0.0',
+  description: 'Video player integration for Shikimori',
+  permissions: [],
   host_permissions: [
     '*://*.shiki.one/*',
     '*://*.shikimori.one/*',
@@ -21,12 +19,12 @@ const manifest = defineManifest({
         '*://shikimori.me/animes/*',
         '*://shiki.one/animes/*'
       ],
-      js: ['src/content.ts'], // Vite сам найдет этот файл
+      js: ['src/content.ts'],
       run_at: 'document_end'
     }
   ]
 });
-// ...
+
 export default defineConfig({
   plugins: [crx({ manifest })],
 });
